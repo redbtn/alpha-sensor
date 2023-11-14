41cbd063a8c6264cd51efb2ef95e3c8008e3ee4c
redbtn <redbtn.id@gmail.com>
2023-11-14 17:11:59 +0800

    Update start_flask.sh

--- start_flask.sh
+++ start_flask.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+
+exec > /var/log/start_flask.log 2>&1
+
+# Change permissions on /dev/ttyACM0
 chmod a+rw /dev/ttyACM0
-cd /apps
-export FLASK_ENV=development
+
+# Change to the directory where your Python script is located
+cd /root/alpha-sensor/
+
+# Start your Python script in the background and detach it from the terminal
+nohup python3 read_arduino.py &
+
+# Set environment variables for Flask
+#export FLASK_ENV=development
 export FLASK_APP=dash.py
-python3 read_arduino.py &
+
+# Start the Flask app, binding it to all network interfaces
 flask run --host=0.0.0.0 &
+
+# Log 
+# echo "Start Flask" >> /var/log/start_flask.log
+
+exit 0
